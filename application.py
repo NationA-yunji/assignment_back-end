fb5557d6098c16b080d1ca609a9420e9e728b3ef
NationA-yunji <yj.kim@nation-a.com>
2023-09-15 13:53:18 +0900

    Docs: 환경변수 지정 및 코드 적용

--- application.py
+++ application.py
@@ -1,29 +1,29 @@
 from flask import Flask, jsonify
 import boto3
+from decouple import config
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-# AWS 계정 및 리전 설정
-aws_access_key_id = '' # 발급 권한 없음
-aws_secret_access_key = '' # 발급 권한 없음
-aws_region = 'ap-northeast-2'
-dynamodb_table_name = 'Music'
+AWS_ACCESS_KEY_ID     = config("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+AWS_REGION           = config("AWS_REGION")
+DYNAMODB_TABLE = 'Music'
 
 # DynamoDB 클라이언트 생성
 dynamodb = boto3.client(
     'dynamodb',
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key,
-    region_name=aws_region
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=AWS_REGION
 )
 
 @app.route('/api/getData')
 def get_dynamodb_data():
     try:
         # DynamoDB 테이블에서 데이터 가져오기
-        response = dynamodb.scan(TableName=dynamodb_table_name)
+        response = dynamodb.scan(TableName=DYNAMODB_TABLE)
         items = response.get('Items', [])
 
         # 데이터를 JSON으로 변환하여 반환
