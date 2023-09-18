c2d7f7f0098195c023e1dc136d6134fda2ef27a8
NationA-yunji <yj.kim@nation-a.com>
2023-09-18 17:29:48 +0900

    Feat: S3 연결 및 env 경로 설정

--- application.py
+++ application.py
@@ -1,15 +1,20 @@
 from flask import Flask, jsonify
 import boto3
-from decouple import config
+from decouple import Config, RepositoryEnv
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
+config = Config(RepositoryEnv('./.env'))
+
 AWS_ACCESS_KEY_ID     = config("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
 AWS_REGION           = config("AWS_REGION")
 DYNAMODB_TABLE = 'Music'
+
+S3_ACCESS_KEY_ID     = config("S3_ACCESS_KEY_ID")
+S3_SECRET_ACCESS_KEY = config("S3_SECRET_ACCESS_KEY")
 
 # DynamoDB 클라이언트 생성
 dynamodb = boto3.client(
@@ -19,6 +24,29 @@ dynamodb = boto3.client(
     region_name=AWS_REGION
 )
 
+# S3 클라이언트 생성
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=S3_ACCESS_KEY_ID,
+    aws_secret_access_key=S3_SECRET_ACCESS_KEY,
+    region_name=AWS_REGION)
+
+# S3 버킷 목록 가져오기
+response = s3.list_buckets()
+for bucket in response['Buckets']:
+    print('Bucket Name:', bucket['Name'])
+    
+# 특정 버킷의 객체 리스트 가져오기
+bucket_name = 'blitz-data-ec2'  # 버킷 이름
+prefix = 'S3-data'  # 가져올 객체들이 저장된 폴더 경로 또는 프리픽스
+
+response = s3.list_objects(Bucket=bucket_name, Prefix=prefix)
+
+# 객체 리스트 출력
+for obj in response.get('Contents', []):
+    print('Object Key:', obj['Key'])
+
+# 라우팅
 @app.route('/')
 def hello_world():
     return 'Hello World!'
